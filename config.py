963a7a8d7c49f9062868b47d0d62bd6e82ed9fdb
Marcos Gabriel <marcosgabriel166@gmail.com>
2026-06-10 16:58:06 -0300

    :rocket: chore: prepara aplicação para deploy

--- config.py
+++ config.py
@@ -1,10 +1,19 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+BASE_DIR = os.path.abspath(
+    os.path.dirname(__file__)
+)
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = (
+
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
         f"sqlite:///{os.path.join(BASE_DIR, 'instance', 'database.db')}"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SECRET_KEY = os.getenv(
+        "SECRET_KEY",
+        "dev-secret-key"
+    )
