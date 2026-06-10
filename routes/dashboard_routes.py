92f381e103faafed5958c6451369c6df1015c91f
Marcos Gabriel <marcosgabriel166@gmail.com>
2026-06-10 14:07:30 -0300

    :sparkles: feat: implementa indicadores dinâmicos no dashboard

--- routes/dashboard_routes.py
+++ routes/dashboard_routes.py
@@ -3,6 +3,11 @@ from flask import (
     render_template
 )
 
+from models import (
+    Cliente,
+    Atendimento
+)
+
 dashboard_bp = Blueprint(
     "dashboard",
     __name__
@@ -12,6 +17,28 @@ dashboard_bp = Blueprint(
 @dashboard_bp.route("/dashboard")
 def dashboard():
 
+    total_clientes = Cliente.query.count()
+
+    prioritarios = Atendimento.query.filter_by(
+        classificacao="Cliente Prioritário"
+    ).count()
+
+    acompanhamento = Atendimento.query.filter_by(
+        classificacao="Cliente em Acompanhamento"
+    ).count()
+
+    analise = Atendimento.query.filter_by(
+        classificacao="Cliente em Análise"
+    ).count()
+
     return render_template(
-        "dashboard/index.html"
+        "dashboard/index.html",
+
+        total_clientes=total_clientes,
+
+        prioritarios=prioritarios,
+
+        acompanhamento=acompanhamento,
+
+        analise=analise
     )
